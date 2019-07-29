75372e65c80226e831decff16e7739df4f691884
Caner Gençdoğan <gencdogancaner@gmail.com>
2019-07-29 13:09:58 +0300

    Kullanıcı Profili

---
+++ RestaurantReviews.WebUI/Areas/Member/Models/Member/PublicProfileVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantReviews.WebUI.Areas.Member.Models {
+    public class PublicProfileVm {
+        public string AppUserId { get; set; }
+        public string Username { get; set; }
+        public string ProfilePicPath { get; set; }
+        public int ReviewCount { get; set; }
+        public string LikedRestaurantName { get; set; }
+        public int LikedRestaurantId { get; set; }
+        public byte? LikedRestaurantScore { get; set; }
+        public string DislikedRestaurantName { get; set; }
+        public int DislikedRestaurantId { get; set; }
+        public byte? DislikedRestaurantScore { get; set; }
+    }
+}
